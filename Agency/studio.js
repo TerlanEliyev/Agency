d044197a8d43aab44d0c643e12191c71507ef7d2
TerlanEliyev <eliyev91terlan91@gmail.com>
2023-03-26 20:24:55 +0400

    Agency
    Agency is a one page HTML/CSS theme built with Bootstrap 5. This theme is perfect for agencies or small businesses looking to set up a simple landing page for their company. The theme can also be modified and adjusted to fit just about any purpose! Download this free Bootstrap theme now to get started!

---
+++ Agency/studio.js
@@ -0,0 +1,20 @@
+let inf=[
+    {
+        url:"https://startbootstrap.github.io/startbootstrap-agency/assets/img/portfolio/2.jpg",
+        h:"E-Commerce",
+        p:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."
+    },
+    {
+        url:"https://startbootstrap.github.io/startbootstrap-agency/assets/img/portfolio/2.jpg",
+        h:"E-Commerce",
+        p:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."
+        
+    },
+    {
+        url:"https://startbootstrap.github.io/startbootstrap-agency/assets/img/portfolio/2.jpg",
+        h:"E-Commerce",
+        p:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."
+        
+    }
+]
+document.getElementById('js').innerText('inf')
